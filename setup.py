92277a22a2889ddf04d00d3e86baa04905f634e4
jpgard <joshua.patrick.gardner@gmail.com>
2022-09-15 14:10:03 -0700

    intial commit of sekelton files

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tablebench',
+      version='0.0',
+      description='A tabular data benchmarking toolkit.',
+      author='Josh Gardner',
+      author_email='jpgard@cs.washington.edu',
+      packages=['tablebench'],
+     )
