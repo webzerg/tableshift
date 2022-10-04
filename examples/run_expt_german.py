81ead1695a76fbcec0ef3e5ee2fa10fa3350fc1e
jpgard <joshua.patrick.gardner@gmail.com>
2022-10-04 10:31:52 -0700

    implements the domain splitter, with a simple example in the German script.

--- examples/run_expt_german.py
+++ examples/run_expt_german.py
@@ -4,7 +4,8 @@ An example script to train a model on the German credit dataset.
 Usage:
     python examples/run_expt_german.py
 """
-from tablebench.core import RandomSplitter, Grouper, TabularDataset, \
+from tablebench.core import RandomSplitter, DomainSplitter,\
+    Grouper, TabularDataset, \
     TabularDatasetConfig, PreprocessorConfig
 
 from sklearn.ensemble import HistGradientBoostingClassifier
@@ -14,7 +15,11 @@ dataset_config = TabularDatasetConfig()
 
 preprocessor_config = PreprocessorConfig()
 
-splitter = RandomSplitter(test_size=0.1, val_size=0.05, random_state=90127)
+# splitter = RandomSplitter(test_size=0.1, val_size=0.05, random_state=90127)
+splitter = DomainSplitter(val_size=0.05,
+                          random_state=12406,
+                          domain_split_varname="purpose",
+                          domain_split_ood_values=["A41", "A42", "A43"])
 grouper = Grouper({"sex": [1, ], "age": [1, ]}, drop=False)
 german = TabularDataset("german",
                         config=dataset_config,
@@ -23,7 +28,7 @@ german = TabularDataset("german",
                         preprocessor_config=preprocessor_config)
 
 X_tr, y_tr, G_tr = german.get_pandas(split="train")
-
+import ipdb;ipdb.set_trace()
 estimator = HistGradientBoostingClassifier()
 estimator.fit(X_tr, y_tr)
 
