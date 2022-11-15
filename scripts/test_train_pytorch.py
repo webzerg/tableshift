835f4034c58bd7b641edfafb140dc3cd11be9347
jpgard <joshua.patrick.gardner@gmail.com>
2022-11-15 11:46:38 -0500

    basic implementation of the resnet baseline of "Revisiting Deep Learning Models for Tabular Data" via rtdl

---
+++ scripts/test_train_pytorch.py
@@ -0,0 +1,34 @@
+import argparse
+import rtdl
+import torch
+import torch.nn.functional as F
+from tablebench.core import TabularDataset, TabularDatasetConfig
+
+from tablebench.datasets.experiment_configs import EXPERIMENT_CONFIGS
+from tablebench.models import get_estimator
+
+experiment = "adult"
+expt_config = EXPERIMENT_CONFIGS[experiment]
+
+dataset_config = TabularDatasetConfig()
+dset = TabularDataset(experiment,
+                      config=dataset_config,
+                      splitter=expt_config.splitter,
+                      grouper=expt_config.grouper,
+                      preprocessor_config=expt_config.preprocessor_config,
+                      **expt_config.tabular_dataset_kwargs)
+train_loader = dset.get_dataloader("train", 512)
+loaders = {s: dset.get_dataloader(s, 512) for s in ("validation", "test")}
+
+model = get_estimator("resnet", d_in=dset.X_shape[1])
+
+lr = 0.001
+weight_decay = 0.0
+loss_fn = F.binary_cross_entropy_with_logits
+
+optimizer = (
+    model.make_default_optimizer()
+    if isinstance(model, rtdl.FTTransformer)
+    else torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
+)
+model.fit(train_loader, optimizer, loss_fn, n_epochs=2, other_loaders=loaders)
---
+++ tablebench/models/rtdl.py
@@ -0,0 +1,64 @@
+from typing import Mapping, Optional
+import rtdl
+import sklearn
+import numpy as np
+import scipy
+import torch
+
+
+# def apply_model(x_num, x_cat=None):
+#     if isinstance(model, rtdl.FTTransformer):
+#         return model(x_num, x_cat)
+#     elif isinstance(model, (rtdl.MLP, rtdl.ResNet)):
+#         assert x_cat is None
+#         return model(x_num)
+#     else:
+#         raise NotImplementedError(
+#             f'Looks like you are using a custom model: {type(model)}.'
+#             ' Then you have to implement this branch first.'
+#         )
+
+
+@torch.no_grad()
+def evaluate(model, loader):
+    model.eval()
+    prediction = []
+    label = []
+    for (batch_x, batch_y, _) in loader:
+        prediction.append(model(batch_x))
+        label.append(batch_y.squeeze())
+    prediction = torch.cat(prediction).squeeze(1).cpu().numpy()
+    target = torch.cat(label).squeeze().cpu().numpy()
+
+    prediction = np.round(scipy.special.expit(prediction))
+    score = sklearn.metrics.accuracy_score(target, prediction)
+    return score
+
+
+class ResNetModel(rtdl.ResNet):
+    def fit(self,
+            train_loader: torch.utils.data.DataLoader,
+            optimizer: torch.optim.Optimizer,
+            loss_fn,
+            n_epochs=1,
+            other_loaders: Optional[
+                Mapping[str, torch.utils.data.DataLoader]] = None):
+
+        for epoch in range(1, n_epochs + 1):
+            for iteration, (x_batch, y_batch, _) in enumerate(train_loader):
+                self.train()
+                optimizer.zero_grad()
+                loss = loss_fn(self(x_batch).squeeze(1), y_batch)
+                loss.backward()
+                optimizer.step()
+                print(f'(epoch) {epoch} (batch) {iteration} '
+                      f'(loss) {loss.item():.4f}')
+            split_scores = {}
+            log_str = f'Epoch {epoch:03d}'
+            if other_loaders:
+                for split, loader in other_loaders.items():
+                    split_score = evaluate(self, loader)
+                    split_scores[split] = split_score
+                    log_str += f' | {split} score: {split_score:.4f}'
+
+            print(log_str, end='')
