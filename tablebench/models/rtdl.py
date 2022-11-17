b482c33ae7fd9582c096df5303996e4a54449759
jpgard <joshua.patrick.gardner@gmail.com>
2022-11-17 18:28:26 -0500

    add reportung of metrics to tune, during training loop. (This will allow for e.g. early stopping etc.)

--- tablebench/models/rtdl.py
+++ tablebench/models/rtdl.py
@@ -4,11 +4,14 @@ Wrappers for tabular baseline models from the rtdl package.
 rtdl source: https://github.com/Yura52/rtdl
 """
 
-from typing import Mapping, Optional
-import rtdl
-import sklearn
+from collections import defaultdict
+from typing import Mapping, Optional, Union
+
 import numpy as np
+from ray import tune
+import rtdl
 import scipy
+import sklearn
 import torch
 
 from tablebench.models.compat import SklearnStylePytorchModel
@@ -72,6 +75,13 @@ def predict_proba(model, X):
     return scipy.special.expit(prediction)
 
 
+def append_by_key(from_dict: dict, to_dict: Union[dict, defaultdict]) -> dict:
+    for k, v in from_dict.items():
+        assert (k in to_dict) or (isinstance(to_dict, defaultdict))
+        to_dict[k].append(v)
+    return to_dict
+
+
 class SklearnStyleRTDLModel(SklearnStylePytorchModel):
     def fit(self,
             train_loader: torch.utils.data.DataLoader,
@@ -79,7 +89,13 @@ class SklearnStyleRTDLModel(SklearnStylePytorchModel):
             loss_fn,
             n_epochs=1,
             other_loaders: Optional[
-                Mapping[str, torch.utils.data.DataLoader]] = None):
+                Mapping[str, torch.utils.data.DataLoader]] = None,
+            tune_report_split=None) -> dict:
+        fit_metrics = defaultdict(list)
+
+        if tune_report_split:
+            assert tune_report_split in list(other_loaders.keys()) + ["train"]
+
         for epoch in range(1, n_epochs + 1):
             metrics = train_epoch(self, train_loader=train_loader,
                                   optimizer=optimizer,
@@ -87,6 +103,15 @@ class SklearnStyleRTDLModel(SklearnStylePytorchModel):
             log_str = f'Epoch {epoch:03d} ' + ' | '.join(
                 f"{k} score: {v:.4f}" for k, v in metrics.items())
             print(log_str)
+
+            if tune_report_split:
+                # TODO(jpgard): consider reporting multiple named metrics here.
+                #  e.g. tune.report(mean_acc=x); tune.report(wg_acc=y) etc.
+                tune.report(metrics[tune_report_split])
+
+            fit_metrics = append_by_key(from_dict=metrics, to_dict=fit_metrics)
+
+        return fit_metrics
 
 
 class ResNetModel(rtdl.ResNet, SklearnStyleRTDLModel):
