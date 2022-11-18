15ab760aa5aad4ecc1b6342e3a335229714a9bf0
jpgard <joshua.patrick.gardner@gmail.com>
2022-11-18 10:52:27 -0500

    refactor so that SklearnStylePytorchModel implements a .fit() method, and subclasses only need to implement train_epoch().

--- tablebench/models/compat.py
+++ tablebench/models/compat.py
@@ -1,7 +1,18 @@
 from abc import ABC, abstractmethod
+from collections import defaultdict
+from typing import Optional, Mapping, Union
 
 import numpy as np
+from ray import tune
+import torch
 from torch import nn
+
+
+def append_by_key(from_dict: dict, to_dict: Union[dict, defaultdict]) -> dict:
+    for k, v in from_dict.items():
+        assert (k in to_dict) or (isinstance(to_dict, defaultdict))
+        to_dict[k].append(v)
+    return to_dict
 
 
 class SklearnStylePytorchModel(ABC, nn.Module):
@@ -19,7 +30,38 @@ class SklearnStylePytorchModel(ABC, nn.Module):
         """sklearn-compatible probability prediction function."""
         raise
 
-
     @abstractmethod
-    def fit(self, **kwargs):
+    def train_epoch(self, **kwargs):
+        """Conduct one epoch of training."""
         raise
+
+    def fit(self,
+            train_loader: torch.utils.data.DataLoader,
+            optimizer: torch.optim.Optimizer,
+            loss_fn,
+            n_epochs=1,
+            other_loaders: Optional[
+                Mapping[str, torch.utils.data.DataLoader]] = None,
+            tune_report_split=None) -> dict:
+        fit_metrics = defaultdict(list)
+
+        if tune_report_split:
+            assert tune_report_split in list(other_loaders.keys()) + ["train"]
+
+        for epoch in range(1, n_epochs + 1):
+            metrics = self.train_epoch(train_loader=train_loader,
+                                       optimizer=optimizer,
+                                       loss_fn=loss_fn,
+                                       other_loaders=other_loaders)
+            log_str = f'Epoch {epoch:03d} ' + ' | '.join(
+                f"{k} score: {v:.4f}" for k, v in metrics.items())
+            print(log_str)
+
+            if tune_report_split:
+                # TODO(jpgard): consider reporting multiple named metrics here.
+                #  e.g. tune.report(mean_acc=x); tune.report(wg_acc=y) etc.
+                tune.report(metrics[tune_report_split])
+
+            fit_metrics = append_by_key(from_dict=metrics, to_dict=fit_metrics)
+
+        return fit_metrics
