9612e6b3ff36a2e383b89ffe495d4b1151f64e21
jpgard <joshua.patrick.gardner@gmail.com>
2022-11-18 17:54:18 -0500

    remove unpacking of batch

--- tablebench/core/torchutils.py
+++ tablebench/core/torchutils.py
@@ -8,10 +8,10 @@ class OnDeviceDataLoader(torch.utils.data.DataLoader):
         self.device_ = torch.device(device)
         super().__init__(**kwargs)
 
-    def _move_batch_to_device(self, *batch):
+    def _move_batch_to_device(self, batch):
         return tuple(tens.to(self.device_) for tens in batch)
 
     def __iter__(self):
         batches = iter(self)
         for b in batches:
-            yield self._move_batch_to_device(*b)
+            yield self._move_batch_to_device(b)
