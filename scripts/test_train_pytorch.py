fce39f929b8bf8cc2759e54720c0e122a2af3226
jpgard <joshua.patrick.gardner@gmail.com>
2022-11-18 17:02:23 -0500

    use proper class constructors for all pytorch models

--- scripts/test_train_pytorch.py
+++ scripts/test_train_pytorch.py
@@ -22,18 +22,12 @@ dset = TabularDataset(experiment,
 train_loader = dset.get_dataloader("train", 512, device=device)
 loaders = {s: dset.get_dataloader(s, 2048) for s in ("validation", "test")}
 
-# model = get_estimator("ft_transformer",
-#                       # TODO(jpgard): set these to defaults from the
-#                       #  [gorishniy2021revisiting] paper.
-#                       d_token=8,
-#                       n_blocks=3,
-#                       attention_dropout=0.,
-#                       ffn_d_hidden=1,
-#                       ffn_dropout=0.,
-#                       residual_dropout=0.,
-#                       cat_cardinalities=None,
-#                       n_num_features=dset.X_shape[1])
-model = get_estimator("mlp", d_in=dset.X_shape[1], d_layers=[256, 256])
+# TODO(jpgard): set all architectural defaults here
+#  based on [gorishniy2021revisiting] paper.
+model = get_estimator("ft_transformer",
+                      n_num_features=dset.X_shape[1],
+                      cat_cardinalities=None)
+# model = get_estimator("mlp", d_in=dset.X_shape[1], d_layers=[256, 256])
 # model = get_estimator("resnet", d_in=dset.X_shape[1])
 # model = get_estimator("group_dro", d_in=dset.X_shape[1], d_layers=[256, 256],
 #                       n_groups=dset.n_groups, group_weights_step_size=0.05)
