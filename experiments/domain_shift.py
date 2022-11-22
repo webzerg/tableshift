52f42b44f7db6fb133d2015236e1782688f2ce5e
jpgard <joshua.patrick.gardner@gmail.com>
2022-11-22 10:50:15 -0500

    major refactor; now works with pytorch models

--- experiments/domain_shift.py
+++ experiments/domain_shift.py
@@ -3,7 +3,8 @@ from datetime import datetime
 
 import pandas as pd
 from tablebench.models import get_estimator, PYTORCH_MODELS, SKLEARN_MODELS, \
-    training
+    training, get_pytorch_model_config
+from tablebench.models.utils import get_predictions_and_labels
 
 from tablebench.configs.domain_shift import domain_shift_experiment_configs
 from tablebench.core import DomainSplitter, TabularDataset, \
@@ -42,35 +43,43 @@ def main(experiment, cache_dir, device: str):
             print(f"[WARNING] error initializing dataset for expt {experiment} "
                   f"with {expt_config.domain_split_varname} == {tgt}: {ve}")
             continue
+        for model in ["mlp"]:
+            # for model in list(PYTORCH_MODELS) + list(SKLEARN_MODELS):
 
-        eval_splits = ("test",) if not isinstance(
-            expt_config.splitter, DomainSplitter) else ("id_test", "ood_test")
+            config = get_pytorch_model_config(
+                model, dset) if model in PYTORCH_MODELS else {}
 
-        for model in list(PYTORCH_MODELS) + list(SKLEARN_MODELS):
-            estimator = get_estimator(model, d_out=dset.X_shape[1])
+            estimator = get_estimator(model, **config)
 
             print(f"fitting estimator of type {type(estimator)} with "
                   f"target {expt_config.domain_split_varname} = {tgt}, "
                   f"src {expt_config.domain_split_varname} = {src}")
 
             if model in SKLEARN_MODELS:
-                estimator = training.train_sklearn(estimator, dset, eval_splits)
+                estimator = training.train_sklearn(estimator, dset)
 
             else:
-                estimator = training.train_pytorch(model, dset, device,
-                                                   eval_splits)
+                estimator = training.train_pytorch(estimator, dset, device)
 
+            # Initialize the metrics dict with some experiment metadata.
             metrics = {"estimator": str(type(estimator)),
                        "task": expt_config.tabular_dataset_kwargs[
                            "name"],
                        "domain_split_varname": expt_config.domain_split_varname,
                        "domain_split_ood_values": tgt,
                        }
-            for split in ("id_test", "ood_test"):
-                try:
-                    X_te, _, _ = dset.get_pandas(split=split)
 
-                    y_hat_te = estimator.predict(X_te)
+            for split in dset.eval_split_names:
+                try:
+                    if model in PYTORCH_MODELS:
+                        loader = dset.get_dataloader(split)
+                        y_hat_te, _ = get_predictions_and_labels(estimator,
+                                                                 loader)
+
+                    else:
+                        X_te, _, _ = dset.get_pandas(split=split)
+                        y_hat_te = estimator.predict(X_te)
+
                     split_metrics = dset.evaluate_predictions(y_hat_te,
                                                               split=split)
                     metrics.update(split_metrics)
@@ -81,10 +90,11 @@ def main(experiment, cache_dir, device: str):
                     continue
             iterates.append(metrics)
 
-        results = pd.DataFrame(iterates)
-        results.to_csv(
-            f"results-{experiment}-{str(datetime.now()).replace(' ', '')}.csv")
-        return
+    results = pd.DataFrame(iterates)
+    fp = f"results-{experiment}-{str(datetime.now()).replace(' ', '')}.csv"
+    print(f"[INFO] writing results to {fp}")
+    results.to_csv(fp)
+    return
 
 
 if __name__ == "__main__":
@@ -95,6 +105,6 @@ if __name__ == "__main__":
     parser.add_argument("--experiment",
                         choices=list(
                             domain_shift_experiment_configs.keys()),
-                        default="brfss_diabetes_st")
+                        default="mooc_course")
     args = parser.parse_args()
     main(**vars(args))
