65cfc322e77f18aa6c5a8cd20b527956a69299a2
jpgard <joshua.patrick.gardner@gmail.com>
2022-11-22 15:25:52 -0500

    rename sensitive_feature_colnames -> domain_feature_colname

--- tablebench/models/expgrad.py
+++ tablebench/models/expgrad.py
@@ -14,32 +14,33 @@ class ExponentiatedGradient(fairlearn.reductions.ExponentiatedGradient):
     fairlearn.ExponentiatedGradient.
     """
 
-    def __init__(self, sensitive_feature_colnames: List[str], **kwargs):
+    def __init__(self, domain_feature_colname: str, **kwargs):
         super().__init__(**kwargs)
+        # Note that "sensitive features" is terminology inherited from aif360;
+        # in our application, the "sensitive features" are really the domain
+        # identities.
+
         # We save the names of the sensitive features as a class attribute
         # so that they are not required at train time; this provides a
         # consistent sklearn-style interface for the model.
-        self.sensitive_feature_colnames = sensitive_feature_colnames
+        self.domain_feature_colname = domain_feature_colname
         # The LabelEncoder is used to ensure sensitive features are of
         # numerical type (not string/categorical).
         self.le = LabelEncoder()
 
     def _prepare_x(self, X):
         """Helper function to drop sensitive columns from data matrix."""
-        return X.drop(columns=self.sensitive_feature_colnames)
+        return X.drop(columns=[self.domain_feature_colname])
 
     def fit(self, X, y, sample_weight=None, **kwargs):
         del sample_weight
 
         # Numerically encode the sensitive attribute; fairlearn.reductions
         # does not accept categorical/string-type data.
-        sens = self.le.fit_transform(X[self.sensitive_feature_colnames].values)
+        sens = self.le.fit_transform(X[[self.domain_feature_colname]].values)
 
         X_ = self._prepare_x(X)
-        super().fit(
-            X_.values, y.values,
-            sensitive_features=sens,
-            **kwargs)
+        super().fit(X_.values, y.values, sensitive_features=sens, **kwargs)
 
     def predict(self, X, random_state=None):
         return super().predict(self._prepare_x(X))
