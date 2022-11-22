5b35c1d58ab7f650c78a720609a3808fa8bd0046
jpgard <joshua.patrick.gardner@gmail.com>
2022-11-22 10:49:42 -0500

    move more elements into pytorch config dict; use dset.eval_split_names.

--- tablebench/models/training.py
+++ tablebench/models/training.py
@@ -6,41 +6,42 @@ from tablebench.models import GroupDROModel
 from tablebench.models.dro import group_dro_loss
 
 
-def train_pytorch(model, dset, device, eval_splits=("validation", "test")):
-    """Helper function to train a pytorch model."""
-    train_loader = dset.get_dataloader("train", 512, device=device)
-    eval_loaders = {s: dset.get_dataloader(s, 2048, device=device) for s in
-                    eval_splits}
-
+def train_pytorch(estimator, dset, device):
+    """Helper function to train a pytorch estimator."""
     config = {
         "lr": 0.001,
         "weight_decay": 0.0,
+        "n_epochs": 25,
+        "batch_size": 512,
     }
+    train_loader = dset.get_dataloader("train", 512, device=device)
+    eval_loaders = {s: dset.get_dataloader(s, 2048, device=device) for s in
+                    dset.eval_split_names}
 
     loss_fn = (group_dro_loss
-               if isinstance(model, GroupDROModel)
+               if isinstance(estimator, GroupDROModel)
                else F.binary_cross_entropy_with_logits)
 
     optimizer = (
-        model.make_default_optimizer()
-        if isinstance(model, rtdl.FTTransformer)
-        else torch.optim.AdamW(model.parameters(), lr=config["lr"],
+        estimator.make_default_optimizer()
+        if isinstance(estimator, rtdl.FTTransformer)
+        else torch.optim.AdamW(estimator.parameters(), lr=config["lr"],
                                weight_decay=config["weight_decay"])
     )
-    model.to(device)
-    model.fit(train_loader, optimizer, loss_fn, n_epochs=25,
-              other_loaders=eval_loaders)
-    return
+    estimator.to(device)
+    estimator.fit(train_loader, optimizer, loss_fn, n_epochs=config["n_epochs"],
+                  other_loaders=eval_loaders)
+    return estimator
 
 
-def train_sklearn(estimator, dset, eval_splits=("test",)):
-    """Helper function to train a sklearn-type model."""
+def train_sklearn(estimator, dset):
+    """Helper function to train a sklearn-type estimator."""
     print(f"fitting estimator of type {type(estimator)}")
     X_tr, y_tr, G_tr = dset.get_pandas(split="train")
     estimator.fit(X_tr, y_tr)
     print("fitting estimator complete.")
 
-    for split in eval_splits:
+    for split in dset.eval_split_names:
 
         X_te, _, _ = dset.get_pandas(split=split)
 
@@ -49,4 +50,4 @@ def train_sklearn(estimator, dset, eval_splits=("test",)):
         print(f"metrics on split {split}:")
         for k, v in metrics.items():
             print(f"\t{k:<40}:{v:.3f}")
-    return
+    return estimator
