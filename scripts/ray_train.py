59f99343a5ff8cab38fc8c1dc9542d761adbf85a
jpgard <jpgard@cs.washington.edu>
2022-12-22 15:20:27 -0500

    add more descriptive logging output

--- scripts/ray_train.py
+++ scripts/ray_train.py
@@ -50,10 +50,11 @@ def main(experiment: str, uid: str, model_name: str, cache_dir: str,
 
     results_df = results.get_dataframe()
     print(results_df)
-    results_df.to_csv(f"tune_results_{uid}_{model_name}_{search_alg}.csv",
-                      index=False)
-    print(results.get_best_result())
-    # call fetc_posprocessed() just to match the full training loop
+    fp = f"tune_results_{uid}_{model_name}_{search_alg}.csv"
+    print(f"[INFO] writing completed results to {fp}")
+    results_df.to_csv(fp, index=False)
+
+    # call fetch_postprocessed() just to match the full training loop
     df = fetch_postprocessed_results_df(results)
     print(df)
     return
