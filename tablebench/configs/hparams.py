00db21045ddc8d0300520b71dcd6681adee24036
jpgard <jpgard@cs.washington.edu>
2022-12-27 00:57:08 -0500

    update xgb, lightgbm, and mlp search spaces; remove histgbm search space to ensure histgbm is not being used in experiments.

--- tablebench/configs/hparams.py
+++ tablebench/configs/hparams.py
@@ -1,12 +1,14 @@
 from ray import tune
 
+# Superset of https://arxiv.org/pdf/2106.11959.pdf, Table 15,
+# in order to cover hparams for other searches that derive from this space.
 _DEFAULT_NN_SEARCH_SPACE = {
     "d_hidden": tune.choice([64, 128, 256, 512, 1024]),
     "lr": tune.loguniform(1e-5, 1e-1),
     "n_epochs": tune.qrandint(5, 100, 5),
     "num_layers": tune.randint(1, 8),
     "dropouts": tune.uniform(0., 0.5),
-    "weight_decay": tune.quniform(0., 1., 0.1),
+    "weight_decay": tune.loguniform(1e-6, 1.)
 }
 
 _deepcoral_search_space = {
@@ -16,22 +18,18 @@ _deepcoral_search_space = {
     "loss_lambda": tune.loguniform(1e-5, 1),
 }
 
-_histgbm_search_space = {
-    "learning_rate": tune.choice([0.1, 0.3, 1.0, 2.0]),
-    "max_leaf_nodes": tune.choice([None, 2, 4, 8, 16, 32, 64]),
-    "l2_regularization": tune.choice(
-        [0., 0.00001, 0.0001, 0.001, 0.01, 0.1, 1.]),
-    "max_bins": tune.choice([32, 64, 128, 255]),
-    "min_samples_leaf": tune.choice([1, 2, 4, 8, 16, 32, 64]),
-}
-
+# Similar to XGBoost search space; however, note that LightGBM is not
+# use in the study from which the XGBoost space is derived.
 _lightgbm_search_space = {
-    "learning_rate": tune.choice([0.1, 0.3, 1.0, 2.0]),
-    "num_iterations": tune.choice([64, 128, 256, 512, ]),
-    "reg_lambda": tune.choice([0., 0.00001, 0.0001, 0.001, 0.01, 0.1, 1.]),
+    "learning_rate": tune.loguniform(1e-5, 1.),
     "min_child_samples": tune.choice([1, 2, 4, 8, 16, 32, 64]),
-    "max_depth": tune.choice([-1, 2, 4, 8]),
+    "min_child_weight": tune.loguniform(1e-8, 1e5),
+    "subsample": tune.uniform(0.5, 1),
+    "max_depth": tune.choice([-1] + list(range(1, 31))),
     "colsample_bytree": tune.uniform(0.5, 1),
+    "colsample_bylevel": tune.uniform(0.5, 1),
+    "reg_alpha": tune.loguniform(1e-8, 1e2),
+    "reg_lambda": tune.loguniform(1e-8, 1e2),
 }
 
 _wcs_search_space = {
@@ -39,12 +37,17 @@ _wcs_search_space = {
     "C_discrim": tune.choice([0.001, 0.01, 0.1, 1., 10., 100., 1000.]),
 
 }
-
+# Matches https://arxiv.org/pdf/2106.11959.pdf; see Table 16
 _xgb_search_space = {
-    "max_depth": tune.randint(4, 8),
+    "max_depth": tune.randint(3, 10),
+    "min_child_weight": tune.loguniform(1e-8, 1e5),
+    "subsample": tune.uniform(0.5, 1),
     "colsample_bytree": tune.uniform(0.5, 1),
     "colsample_bylevel": tune.uniform(0.5, 1),
-    "learning_rate": tune.choice([0.1, 0.3, 1.0, 2.0]),
+    "learning_rate": tune.loguniform(1e-5, 1.),
+    "gamma": tune.loguniform(1e-8, 1e2),
+    "lambda": tune.loguniform(1e-8, 1e2),
+    "alpha": tune.loguniform(1e-8, 1e2),
     "max_bin": tune.choice([128, 256, 512])
 }
 
@@ -82,7 +85,6 @@ search_space = {
     "expgrad": _expgrad_search_space,
     "ft_transformer": _ft_transformer_search_space,
     "group_dro": _group_dro_search_space,
-    "histgbm": _histgbm_search_space,
     "lightgbm": _lightgbm_search_space,
     "mlp": _DEFAULT_NN_SEARCH_SPACE,
     "resnet": _resnet_search_space,
