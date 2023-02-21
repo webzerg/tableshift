d789c5f0300536b372fe6714200b6fe74744ddf0
jpgard <joshua.patrick.gardner@gmail.com>
2023-02-21 15:05:06 -0800

    implements get_iid_dataset

--- tablebench/core/__init__.py
+++ tablebench/core/__init__.py
@@ -2,4 +2,4 @@ from .grouper import Grouper
 from .splitter import Splitter, FixedSplitter, RandomSplitter, DomainSplitter
 from .tabular_dataset import TabularDataset, TabularDatasetConfig, CachedDataset
 from .features import PreprocessorConfig
-from .getters import get_dataset
+from .getters import get_dataset, get_iid_dataset
